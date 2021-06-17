0a01ecd6dd752045f60d858cbdb0d0f7ec1f232a
lumsdnb <jeremy@lums.io>
2021-06-17 14:03:01 +0200

    Home page module

---
+++ src/home.js
@@ -0,0 +1,18 @@
+const homePage=()=>{
+    console.log("homepage loaded");
+    const placeholder = document.createElement("div")
+    const headerEl= document.createElement("header")
+    const h1El = document.createElement("h1")
+    h1El.innerHTML="borger palace"
+    const subTitleEl = document.createElement("h2")
+    subTitleEl.innerHTML="your number 1 stop for burgers & more"
+
+    const textBodyEl = document.createElement("p")
+    textBodyEl.innerHTML="we serve up borgers most of the time. we don't have fixed opening hours, since we're usually frying up patties every day, but if you'd like to check if we're open before showing up, feel free to message our telegram bot at"
+    headerEl.append(h1El,subTitleEl)
+    placeholder.classList.add("card")
+    placeholder.append(headerEl, textBodyEl)
+    return placeholder
+}
+
+export default homePage
