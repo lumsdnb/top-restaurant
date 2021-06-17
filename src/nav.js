5aeaff0cee0eff6904a91dbd24bcdb5f999ed15a
lumsdnb <jeremy@lums.io>
2021-06-17 14:03:28 +0200

    Nav bar module

---
+++ src/nav.js
@@ -0,0 +1,23 @@
+//generate nav bar
+//<nav><li>home</li><li>menu</li><li>contact us</li></nav>
+
+const navBar=()=>{
+    const nav = document.createElement("nav")
+    const entries=["home","menu","contact us"]
+    
+    for (let i = 0; i < entries.length; i++) {
+        const element = entries[i];
+        let entry = document.createElement("li")
+        let linkText = document.createTextNode(element);
+        let a = document.createElement('a');
+        a.append(linkText)
+        a.title = element;
+        a.href = `/${element}`;
+        entry.appendChild(a)
+        nav.append(entry)
+    }
+
+    return nav
+}
+
+export default navBar
